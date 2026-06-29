17e3d3c57314cba2a5097a8613cef9a375dafd89
kunwar-005 <kunwarpartap23@gmail.com>
2026-06-29 16:34:01 +0530

    Time: 7 ms (72.68%), Space: 19.7 MB (24.61%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int k=0;
-        int i =0;
-        int j =1;
-        int unique=1;
-        while(j<nums.size()){
-            if(k==0){
-                if(nums[j]==nums[j-1]){
-                    nums[i+1]= nums[j];
-                    k=1;
+        int k = 0;
+        int i = 0;
+        int j = 1;
+        int unique = 1;
+        while (j < nums.size()) {
+            if (k == 0) {
+                if (nums[j] == nums[j - 1]) {
+                    nums[i + 1] = nums[j];
+                    k = 1;
                     i++;
                     j++;
-                }
-                else {
-                    nums[i+1]=nums[j];
+                } else {
+                    nums[i + 1] = nums[j];
                     i++;
                     j++;
                     unique++;
-
                 }
 
-            }
-            else if(k==1){
-                if(nums[j]==nums[j-1]){
+            } else if (k == 1) {
+                if (nums[j] == nums[j - 1]) {
                     j++;
-                }
-                else{
-                    nums[i+1]=nums[j];
-                    k=0;
+                } else {
+                    nums[i + 1] = nums[j];
+                    k = 0;
                     unique++;
                     i++;
                     j++;
@@ -36,6 +32,27 @@ public:
             }
         }
 
-        return i+1;
+        return i + 1;
     }
 };
+
+/*class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        if (nums.size() <= 2)
+            return nums.size();
+
+        int i = 2;
+
+        for (int j = 2; j < nums.size(); j++) {
+
+            if (nums[j] != nums[i - 2]) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+};*/
