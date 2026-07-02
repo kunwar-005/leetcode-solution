f95e33b379d2099fc29be9c1e81b1c7c969c9b6a
kunwar-005 <kunwarpartap23@gmail.com>
2026-07-02 23:50:02 +0530

    Time: 3 ms (45.14%), Space: 10.8 MB (18.74%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -14,3 +14,25 @@ public:
     return true;
     }
 };
+/*
+but in this question we used converted our int to string and string store s eaxh digit there for it makes the space complexity o(n)
+but sometimes in interview we are asked to do this in O(1) space or cosntant space 
+the we use this approach
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+
+        int rev = 0;
+
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (x == rev || x == rev / 10);
+    }
+};  
+*/
